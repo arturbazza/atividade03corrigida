e8e1364ee9578d34657b956518436605464f3a6c
arturbazza <agencia@fonzie.com.br>
2024-04-17 21:49:30 -0300

    CepService

--- src/main/java/br/org/fundatec/atividade03corrigida/service/CepService.java
+++ src/main/java/br/org/fundatec/atividade03corrigida/service/CepService.java
@@ -1,7 +1,20 @@
 package br.org.fundatec.atividade03corrigida.service;
 
 import br.org.fundatec.atividade03corrigida.model.Address;
+import br.org.fundatec.atividade03corrigida.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface CepService {
-    Address findAddressByCep(String cep);
+@Service
+public class CepService {
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    public Address findAddressByCep(String cep) {
+
+        Address address = null;
+
+        return addressRepository.save(address);
+    }
 }
